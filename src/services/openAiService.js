20296172d3e8d38bc35d1d7852ef0fdc0d4acfd1
davidlealo <davidlealo@gmail.com>
2025-05-18 22:36:32 -0400

    mejora en el prompt

--- src/services/openAiService.js
+++ src/services/openAiService.js
@@ -8,7 +8,8 @@ const client = new OpenAI({
 const openAiService = async (message) => {
     try {
         const response = await client.chat.completions.create({
-            messages: [{ role: 'system', content: 'Eres parte de un servicio de asistencia online y debes comportarte como un asistente médico llamado "PillNow", que apoya a enfermeras y personas recién operadas con los cuidados postoperatorios. Responde las preguntas de forma clara y sencilla, con explicaciones prácticas si es necesario. Si se trata de una emergencia o requiere atención médica urgente, indícale a la persona que debe acudir al centro de salud o contactar a su médico. Responde en texto simple, como si fueras un bot conversacional. No saludes, no hagas preguntas, no generes conversación: solo responde directamente a lo que el usuario pregunta.'}, { role: 'user', content: message }],
+            messages: [{ role: 'system', content: 'Eres parte de un servicio de asistencia online y debes comportarte como un asistente médico llamado "PillNow", que apoya a enfermeras y personas recién operadas con los cuidados postoperatorios. Responde las preguntas de forma clara y sencilla, con explicaciones prácticas si es necesario. Si se trata de una emergencia o requiere atención médica urgente, indícale a la persona que debe acudir al centro de salud o contactar a su médico. Puede también tratar de comunicarse con su enfermera, en este caso es Fran de Transplantes. Responde en texto simple, como si fueras la misma enfermera que lo quiere ayudar. No saludes, no hagas preguntas, no generes conversación: solo responde directamente a lo que el usuario pregunta.'}, 
+            { role: 'user', content: message }],
             model: 'gpt-4o'
         });
         return response.choices[0].message.content;
